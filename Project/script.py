f9cae12879a94c2310b182d2a2e35ce224d02598
mike12724 <34524308+mike12724@users.noreply.github.com>
2019-04-05 11:18:53 -0700

    modified

--- Project/script.py
+++ Project/script.py
@@ -2,6 +2,7 @@
 
 import os
 import re #for regex
+import time #for speed testing
 
 def regex_recurse(dirname, exp):
     directory = None
@@ -16,7 +17,7 @@ def regex_recurse(dirname, exp):
         new_path = os.path.join(dirname,item)
         if os.path.isfile(new_path):
             if exp.match(item):
-                passing_list.append(item)
+                passing_list.append(new_path)
 
         elif os.path.isdir(new_path):
             passing_list += regex_recurse(new_path, exp) # if directory
@@ -29,30 +30,45 @@ def test_txt():
     #find all text files
     cwd = os.getcwd()
     exp = re.compile(".*\.txt") #.txt ending
+    
+    now = time.clock()
     out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
     if len(out) == 8:
         print "Test 1 Success!"
-    print "Output: " + str(out) + "\n"
+    print "Output: " + str(out)
+    print "Time: " + str(speed) + " seconds\n"
 
 
 def test_nums():
     cwd = os.getcwd()
     exp = re.compile(".*[0-9].*") #contains at least one number
+    
+    now = time.clock()
     out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
     if len(out) == 5:
         print "Test 2 Success!"
-    print "Output: " + str(out) + "\n"
+    print "Output: " + str(out)
+    print "Time: " + str(speed) + " seconds\n"
     
     
 def test_bad_dir():
     cwd = os.getcwd()
     cwd += "bogus" #bogus directory
-    exp = re.compile(".*\.txt") 
+    exp = re.compile(".*\.txt")
+    
+    now = time.clock()
     out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
     if out is not None:
         print "Test 3 Failed!"
     else:
         print "Test 3 Success!"
+    print "Time: " + str(speed) + " seconds"
     
 
 #################### Main Start ####################
--- Project/script_using_walk.py
+++ Project/script_using_walk.py
@@ -1,5 +1,8 @@
+#Created in python 2.7
+
 import os
 import re #for regex
+import time #for speed testing
 
 def regex_recurse(dirname, exp):
     directory = None
@@ -10,52 +13,62 @@ def regex_recurse(dirname, exp):
         return
 
     passing_list = []
-    for item in directory:
-        new_path = os.path.join(dirname,item)
-        if os.path.isfile(new_path):
-            if exp.match(item):
-                passing_list.append(item)
-
-        elif os.path.isdir(new_path):
-            passing_list += regex_recurse(new_path, exp) # if directory
-
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            if exp.match(name):
+                passing_list.append(os.path.join(root,name))
     return passing_list
-        
 
 ################ TEST CASES #################################
 def test_txt():
     #find all text files
     cwd = os.getcwd()
     exp = re.compile(".*\.txt") #.txt ending
+    
+    now = time.clock()
     out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
+    if len(out) == 8:
+        print "Test 1 Success!"
+    print "Output: " + str(out)
+    print "Time: " + str(speed) + " seconds\n"
 
 
-def test_words():
+def test_nums():
     cwd = os.getcwd()
-
-
+    exp = re.compile(".*[0-9].*") #contains at least one number
+    
+    now = time.clock()
+    out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
+    if len(out) == 5:
+        print "Test 2 Success!"
+    print "Output: " + str(out)
+    print "Time: " + str(speed) + " seconds\n"
+    
     
 def test_bad_dir():
     cwd = os.getcwd()
     cwd += "bogus" #bogus directory
-    exp = re.compile(".*\.txt") 
+    exp = re.compile(".*\.txt")
+    
+    now = time.clock()
     out = regex_recurse(cwd, exp)
+    speed = time.clock()-now
+    
     if out is not None:
-        print "Test Failed!"
+        print "Test 3 Failed!"
     else:
-        print "Test success!"
+        print "Test 3 Success!"
+    print "Time: " + str(speed) + " seconds"
     
 
+#################### Main Start ####################
 
 if __name__ == "__main__":
-    #create examples
-    
-
-
-    
-    nem = "C:/Games/Elsword/"
-    
-
-    #out here put a sanity check for valid directory
-    a = regex_recurse(nem, exp)
+    test_txt()
+    test_nums()
+    test_bad_dir()
                       
